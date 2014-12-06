834b4c28699bd71ebe2ebcb775a9d37c6487ea3e
dragosmc <dragos.ciupureanu@gmail.com>
2014-12-06 09:07:10 -0800

    some stuff

--- econding.py
+++ econding.py
@@ -1,9 +1,126 @@
 __author__ = 'dragosmc'
 
+# typedef struct tagFIS_REG_H2D
+# {
+# 	// DWORD 0
+# 	BYTE	fis_type;	// FIS_TYPE_REG_H2D
+#
+# 	BYTE	pmport:4;	// Port multiplier
+# 	BYTE	rsv0:3;		// Reserved
+# 	BYTE	c:1;		// 1: Command, 0: Control
+#
+# 	BYTE	command;	// Command register
+# 	BYTE	featurel;	// Feature register, 7:0
+#
+# 	// DWORD 1
+# 	BYTE	lba0;		// LBA low register, 7:0
+# 	BYTE	lba1;		// LBA mid register, 15:8
+# 	BYTE	lba2;		// LBA high register, 23:16
+# 	BYTE	device;		// Device register
+#
+# 	// DWORD 2
+# 	BYTE	lba3;		// LBA register, 31:24
+# 	BYTE	lba4;		// LBA register, 39:32
+# 	BYTE	lba5;		// LBA register, 47:40
+# 	BYTE	featureh;	// Feature register, 15:8
+#
+# 	// DWORD 3
+# 	BYTE	countl;		// Count register, 7:0
+# 	BYTE	counth;		// Count register, 15:8
+# 	BYTE	icc;		// Isochronous command completion
+# 	BYTE	control;	// Control register
+#
+# 	// DWORD 4
+# 	BYTE	rsv1[4];	// Reserved
+# } FIS_REG_H2D;
 
-feature = ""
-count = ""
-lba = ""
-device = ""
-command = "\xEC"
+class regH2d:
+    def __init__(self):
+        # DWORD 0
+        self.fis_type = 0x34
+
+        self.pmport = 0x0
+        self.c = 0x01
+
+        self.command = 0xEC
+        self.feature = 0x00
+
+        # DWORD 1
+        self.lba0 = 0x00
+        self.lba1 = 0x00
+        self.lba2 = 0x00
+        self.device = 0x0
+
+        # DWORD 2
+        self.lba3 = 0x00
+        self.lba4 = 0x00
+        self.lba5 = 0x00
+        self.feature = 0x00
+
+        # DWORD 3
+        self.countl = 0x00
+        self.counth = 0x00
+        self.icc = 0x00
+        self.control = 0x00
+
+        # DWORD 4
+        self.rsv1 = 0x0
+
+
+    def to_byte_array(self):
+        btrl = bytearray(self.fis_type)
+        print binascii.hexlify(btrl)
+
+        temp = bytearray(self.pmport)
+        btrl = btrl + temp
+
+        temp = bytearray(self.c)
+        btrl = btrl + temp
+
+        temp = bytearray(self.command)
+        btrl = btrl + temp
+
+        temp = bytearray(self.feature)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba0)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba1)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba2)
+        btrl = btrl + temp
+
+        temp = bytearray(self.device)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba3)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba4)
+        btrl = btrl + temp
+
+        temp = bytearray(self.lba5)
+        btrl = btrl + temp
+
+        temp = bytearray(self.feature)
+        btrl = btrl + temp
+
+        temp = bytearray(self.countl)
+        btrl = btrl + temp
+
+        temp = bytearray(self.counth)
+        btrl = btrl + temp
+
+        temp = bytearray(self.icc)
+        btrl = btrl + temp
+
+        temp = bytearray(self.control)
+        btrl = btrl + temp
+
+        return btrl
+
+
+
 
--- hdd.py
+++ hdd.py
@@ -4,18 +4,27 @@ import usb.core
 import usb.util
 import usb.control
 import usb.backend
-import base64
+import binascii
+from array import array
 interface = 0
 
 
 def get_devices():
     dev_list = usb.core.find(idVendor=0x152d, idProduct=0x2339)
-    print dev_list
+    #print dev_list
     return dev_list
 
 dev = get_devices()
+#dev.set_configuration()
+if dev.is_kernel_driver_active(0) is True:
+    print "but we need to detach kernel driver"
+    dev.detach_kernel_driver(interface)
+    print "claiming device"
+    usb.util.claim_interface(dev, interface)
+
 cfg = dev.get_active_configuration()
 # cfg.set()
+#print cfg
 intf = cfg[(0,0)]
 
 ep = usb.util.find_descriptor(
@@ -27,25 +36,55 @@ ep = usb.util.find_descriptor(
         usb.util.ENDPOINT_OUT)
 
 assert ep is not None
-dev.write(ep.bEndpointAddress,'\xEC')
-collected = 0
-attempts = 50
-while collected < attempts:
-    data =  dev.read(ep.bEndpointAddress,ep.wMaxPacketSize )
-    print data
-    collected+=1
 
-# print ep.bEndpointAddress
+#print data
+  
+
+print ep.bEndpointAddress
 # print ep.wMaxPacketSize
 
 # print "Wrote to the first endpoint"
 # print ep.read(512,0)
 
-if dev.is_kernel_driver_active(0) is True:
-            print "but we need to detach kernel driver"
-            dev.detach_kernel_driver(interface)
-            print "claiming device"
-            usb.util.claim_interface(dev, interface)
+
+
+# feature = bytearray([0xD0])
+# count = bytearray([0x00])
+# lba = bytearray([0x00])
+# device = bytearray([0x00])
+# lbal = bytearray([0x00])
+# lbam= bytearray([0x4F])
+# lbah=bytearray([0xC2])
+
+feature = bytearray([0x00])
+count = bytearray([0x00])
+lba = bytearray([0x00])
+device = bytearray([0x10])
+lbal = bytearray([0x00])
+lbam= bytearray([0x00])
+lbah=bytearray([0x00])
+command = bytearray([0xE6]) #IDENTIFY DEVICE '\xEC'
+c = device +command
+# c =  device +command
+# c = command + device +feature
+print binascii.hexlify(c)
+print ep.bEndpointAddress
+#print(ep.write(command,0))
+# print(ep.read(512))
+print(dev.write(0x2,c,0))
+print(dev.read(0x81,512,1000))
+
+# for bRequest in range(255):
+#     try:
+#         read = dev.ctrl_transfer(0x81, bRequest, 0, 0, 8) #read 8 bytes
+#         print "bRequest ", bRequest
+#         print read
+#     except:
+#         # failed to get data for this request
+#         pass
+#
+# data =  dev.read(0x81,ep.wMaxPacketSize)
+# print data
 
 #             dev.write(1,'\xEC')
 #             print "WRITEEEEEE"
@@ -53,10 +92,10 @@ if dev.is_kernel_driver_active(0) is True:
             # ep.write('\xEC')
 
 
-           # print "release claimed interface"
-           # usb.util.release_interface(dev, interface)
-           # print "now attaching the kernel driver again"
-           # dev.attach_kernel_driver(interface)
-           # print "all done"
+  #print "release claimed interface"
+  #usb.util.release_interface(dev, interface)
+  #print "now attaching the kernel driver again"
+  #dev.attach_kernel_driver(interface)
+  #print "all done"
 
 
